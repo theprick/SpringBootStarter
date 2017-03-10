29a13c20e2e203e8844eca8a18080de7cad0e35b
Popescu Adrian <padrian@gmail.com>
2017-03-11 01:26:12 +0200

    Added spring boot configuration

--- src/main/java/home/adipopescu/springbootquickstart/CourseApiApp.java
+++ src/main/java/home/adipopescu/springbootquickstart/CourseApiApp.java
@@ -1,7 +1,15 @@
 package home.adipopescu.springbootquickstart;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by Adi on 11.03.2017.
  */
+@SpringBootApplication
 public class CourseApiApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseApiApp.class);
+    }
 }
